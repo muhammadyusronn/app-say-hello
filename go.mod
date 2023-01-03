2ded18f827ad311f8a1c7e2e064ba2165bd3078b
Muhammad Yusron Hartoyo <muhammadyusron733@gmail.com>
2023-01-03 22:05:36 +0700

    INIT

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/muhammadyusronn/app-say-hello
+
+go 1.19
+
+require github.com/muhammadyusronn/go-say-hello/v2 v2.0.0-20230103150151-0aee7e52b5ea // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/muhammadyusronn/go-say-hello/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.Hello("Yusron", "Jakarta"))
+}
